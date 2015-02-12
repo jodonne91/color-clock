28a3fdf58329fa3dde3b28ac625db2c1e2886374
jodonne91 <jodonne91@gmail.com>
2015-02-12 11:22:54 -0500

    Added html, styling, and javascript/jQuery for color clock

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+
+
+$(document).on("ready", function() {
+  setInterval(setTime, 1000);
+})
+
+function setTime() 
+{
+  
+  var color = getColor();
+  var time = getTime();
+
+
+  var clockTime = "<h1 class = 'clock'>" + time + "</h1>";
+
+  $(".background").css("background-color", color);
+
+  $(".clock").remove()
+  $(".clockDiv").append(clockTime)
+
+}
+
+
+function getColor() 
+{
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+
+  var hour = Math.round((h / 23) * 255)
+  var minute = Math.round((m / 59) * 255);
+  var sec = Math.round((s / 59) * 255)
+
+  var color = 'rgb(' + hour + ',' + minute + ',' + sec + ')';
+
+  //console.log(color)
+
+  return color;
+
+}
+
+
+
+function getTime() 
+{
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+
+  if( h < 10 ){ h = '0' + h; }
+  if( m < 10 ){ m = '0' + m; }
+  if( s < 10 ){ s = '0' + s; }
+
+  var clockTime = h + ":" + m + ":" + s;
+
+  return clockTime;
+
+}
+
+
+
 /*
   This function takes in two parameters: a number, and a string.
   The number represents the amount of hours/minutes/seconds.
